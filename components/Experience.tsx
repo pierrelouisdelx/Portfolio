813439f56eba8b9e2aa265246a0ae5a019341ac1
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-06-05 01:36:23 +0200

    Experience fixed
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -2,6 +2,7 @@ import { VerticalTimeline } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import ExperienceCard from '@/components/Experience/WorkCard';
 import { useState } from 'react';
+import { Title } from '@/ui';
 
 const workexperiences = [
     {
@@ -43,40 +44,43 @@ const freelanceexperiences = [
     },
 ];
 
+function classNames(...classes: any[]) {
+    return classes.filter(Boolean).join(' ');
+}
+
 export default function Experience() {
-    const [activeTab, setActiveTab] = useState('work');
+    const [activeTab, setActiveTab] = useState(0);
 
     return (
-        <>
-            <div className='mb-4 border-b border-gray-200 dark:border-gray-700'>
-                <ul
-                    className='flex text-sm font-medium text-center'
-                    id='myTab'
-                    data-tabs-toggle='#myTabContent'
-                    role='tablist'
-                >
-                    <li className='mr-2' role='presentation'>
+        <div className='flex flex-col'>
+            <Title>Experience</Title>
+            <div
+                className='mb-4 border-b border-gray-200 dark:border-gray-700 flex justify-center'
+                id='experience'
+            >
+                <ul className='flex text-sm font-medium text-center'>
+                    <li className='mr-2'>
                         <button
-                            className='inline-block p-4 border-b-2 rounded-t-lg'
-                            id='profile-tab'
-                            data-tabs-target='#work'
-                            type='button'
-                            role='tab'
-                            aria-controls='work'
-                            aria-selected='true'
+                            className={classNames(
+                                'inline-block p-4 border-b-2 rounded-t-lg',
+                                activeTab === 0
+                                    ? 'border-gray-600 text-white dark:border-gray-300 dark:text-gray-300'
+                                    : 'border-transparent text-gray-400 hover:border-gray-300 dark:hover:text-gray-300'
+                            )}
+                            onClick={() => setActiveTab(0)}
                         >
                             Work Experience
                         </button>
                     </li>
-                    <li className='mr-2' role='presentation'>
+                    <li className='mr-2'>
                         <button
-                            className='inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
-                            id='freelance-tab'
-                            data-tabs-target='#freelance'
-                            type='button'
-                            role='tab'
-                            aria-controls='freelance'
-                            aria-selected='false'
+                            className={classNames(
+                                'inline-block p-4 border-b-2 rounded-t-lg',
+                                activeTab === 1
+                                    ? 'border-gray-600 text-white dark:border-gray-300 dark:text-gray-300'
+                                    : 'border-transparent text-gray-400 hover:border-gray-300 dark:hover:text-gray-300'
+                            )}
+                            onClick={() => setActiveTab(1)}
                         >
                             Freelance Experience
                         </button>
@@ -84,12 +88,7 @@ export default function Experience() {
                 </ul>
             </div>
             <div id='myTabContent'>
-                <div
-                    className='hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800'
-                    id='work'
-                    role='tabpanel'
-                    aria-labelledby='work-tab'
-                >
+                <div className={classNames('p-4 rounded-lg', activeTab === 0 ? 'block' : 'hidden')}>
                     <VerticalTimeline animate={true} lineColor={'#fb923c'}>
                         {workexperiences.map((experience) => (
                             <ExperienceCard {...experience} key={experience.company} />
@@ -97,12 +96,7 @@ export default function Experience() {
                     </VerticalTimeline>
                 </div>
 
-                <div
-                    className='hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800'
-                    id='freelance'
-                    role='tabpanel'
-                    aria-labelledby='freelance-tab'
-                >
+                <div className={classNames('p-4 rounded-lg', activeTab === 1 ? 'block' : 'hidden')}>
                     <VerticalTimeline animate={true} lineColor={'#fb923c'}>
                         {freelanceexperiences.map((experience) => (
                             <ExperienceCard {...experience} key={experience.company} />
@@ -110,6 +104,6 @@ export default function Experience() {
                     </VerticalTimeline>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
