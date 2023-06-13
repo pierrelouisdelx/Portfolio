91b85a4e97684c0e6f4c8c1e2b690dd6f6c41aeb
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-06-14 00:23:40 +0200

    link added to experince
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Experience/WorkCard.tsx
+++ components/Experience/WorkCard.tsx
@@ -1,7 +1,9 @@
 import { VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
 
 interface ExperienceCardProps {
     company: string;
+    link?: string;
     position: string;
     location: string;
     date: string;
@@ -26,9 +28,16 @@ export default function ExperienceCard(props: ExperienceCardProps) {
             date={props.date}
             dateClassName='text-gray-400'
         >
-            <h3 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 vertical-timeline-element-title dark:text-white'>
-                {props.company}
-            </h3>
+            <div className='flex items-center'>
+                <h3 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 vertical-timeline-element-title dark:text-white'>
+                    {props.company}
+                </h3>
+                {props.link ? (
+                    <a href={props.link} target='_blank' rel='noreferrer'>
+                        <ArrowTopRightOnSquareIcon className='w-6 h-6 pb-1 ml-2 text-orange-400 cursor-pointer' />
+                    </a>
+                ) : null}
+            </div>
             <h4 className='mb-3 font-normal text-gray-700 vertical-timeline-element-subtitle dark:text-gray-400'>
                 {props.position} - {props.location}
             </h4>
