5529f939c8f8fd4297487add713ff8db960a0168
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-06-16 23:20:34 +0200

    contacts added
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,20 +1,46 @@
 import Image from 'next/image';
 import Title from '@/ui/Title';
 
-const contacts = [
+const socials = [
     {
         image: '/linkedin.png',
-        link: 'https://linkedin.com'
-    }
+        link: 'https://linkedin.com',
+    },
+    {
+        image: '/github.png',
+        link: 'https://github.com/pierrelouisdelx',
+    },
 ];
 
 export default function Contacts() {
     return (
-        <div className='flex flex-col w-full' id='contacts'>
+        <div className='flex flex-col w-full h-full' id='contacts'>
             <Title>Contacts</Title>
+            <div className='flex items-center justify-center h-full'>
+                <div className='flex flex-col justify-center lg:w-1/2'>
+                    <div className='flex flex-wrap justify-between md:flex-row'>
+                        <input
+                            className='w-full p-2 m-2 text-black border rounded-lg md:max-w-1/2'
+                            placeholder='Full Name'
+                        />
+                        <input
+                            className='w-full p-2 m-2 text-black border rounded-lg md:max-w-1/2'
+                            placeholder='Email'
+                        />
+                    </div>
+                    <textarea
+                        className='w-full p-2 m-2 text-black border rounded-lg'
+                        placeholder='Message'
+                        rows={5}
+                    />
+                    <button className='w-1/2 p-2 m-2 text-orange-400 border border-orange-400 rounded-lg cursor-pointer hover:bg-orange-400 hover:text-white'>
+                        Send
+                    </button>
+                </div>
+            </div>
             <div className='flex flex-row justify-evenly'>
-                {contacts.map((contact) => (
-                    <Contact {...contact} key={contact.link} />
+                {socials.map((contact) => (
+                    <Social {...contact} key={contact.link} />
                 ))}
             </div>
         </div>
@@ -26,7 +52,7 @@ interface ContactProps {
     link: string;
 }
 
-const Contact = (props: ContactProps) => {
+const Social = (props: ContactProps) => {
     return (
         <a href={props.link} target='_blank'>
             <Image src={props.image} width={50} height={50} alt={props.image} />
