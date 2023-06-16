b52cf17194df2451cd30785f1668c84537bf47e6
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-06-16 23:32:50 +0200

    Card blur on hover added
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Projects/Card.tsx
+++ components/Projects/Card.tsx
@@ -1,7 +1,5 @@
-import { motion, MotionValue, useTransform } from 'framer-motion';
-import { useRef } from 'react';
-import Link from 'next';
-import Image from 'next/image';
+import { motion, MotionValue } from 'framer-motion';
+import { useState } from 'react';
 
 interface ProjectProps {
     name: string;
@@ -21,17 +19,31 @@ export const Card = ({
     mouseX: MotionValue<number>;
     mouseY: MotionValue<number>;
 }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
         <motion.div
             layout
-            className='relative w-full max-w-md p-5 m-2 text-left cursor-pointer lg:w-1/4 h-72'
+            className='relative w-full max-w-md p-5 m-2 text-left cursor-pointer lg:w-1/4'
         >
             <img
                 src={project.background}
                 alt={project.name}
-                className='absolute inset-0 border rounded-lg -z-10'
+                className={`absolute inset-0 border rounded-lg -z-10 ${
+                    isHovered ? 'blur-sm' : ''
+                } transition-all ease-in-out duration-300`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
             />
-            <a href={project.url} target='_blank'>
+            <a
+                href={project.url}
+                target='_blank'
+                className={`w-full h-full ${
+                    isHovered ? 'opacity-100' : 'opacity-0'
+                } transition-all ease-in-out duration-300`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
                 <h3 className='pb-3 text-2xl font-semibold'>{project.name}</h3>
                 <p>{project.description}</p>
                 <p className='pt-2 italic text-slate-500'>{project.technologies.join(', ')}</p>
