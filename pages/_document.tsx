939192bc9ee2278023e16dbef72040dd45da2ce1
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-06-19 00:44:23 +0200

    head title added
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang='en'>
+            <Head>
+                <meta name='theme-color' content='#000000' />
+                <title>Portfolio</title>
+                <meta name='description' content='Portfolio' />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
