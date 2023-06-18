dcbd781e3865e24e98c5c50d5fbc8bc27968f89a
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-06-18 17:20:41 +0200

    isSelected project added
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Projects/Card.tsx
+++ components/Projects/Card.tsx
@@ -8,39 +8,44 @@ interface ProjectProps {
     url: string;
     category: number;
     background?: string;
+    height?: number;
 }
 
 export const Card = ({
     project,
-    mouseX,
-    mouseY,
+    setSelectedProject,
 }: {
     project: ProjectProps;
-    mouseX: MotionValue<number>;
-    mouseY: MotionValue<number>;
+    setSelectedProject: (index: any) => void;
 }) => {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
         <motion.div
             layout
-            className='relative w-full max-w-md p-5 m-2 text-left cursor-pointer lg:w-1/4'
+            className='relative m-2 text-left border rounded-lg cursor-pointer'
+            style={{
+                width: '100%',
+                maxWidth: '24rem',
+                gridRowEnd: getGridRowEnd(project?.height),
+                gridTemplateColumns: 'repeat(auto-fit, minmax(0, 1fr))',
+                gridTemplateRows: 'repeat(auto-fit, minmax(0, 1fr))',
+            }}
+            onClick={() => setSelectedProject(project)}
         >
             <img
                 src={project.background}
                 alt={project.name}
-                className={`absolute inset-0 border rounded-lg -z-10 ${
-                    isHovered ? 'blur-sm' : ''
-                } transition-all ease-in-out duration-300`}
+                className={`${
+                    isHovered ? 'blur-sm brightness-50' : ''
+                } transition-all ease-in-out duration-300 rounded-lg`}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             />
             <a
-                href={project.url}
-                target='_blank'
                 className={`w-full h-full ${
                     isHovered ? 'opacity-100' : 'opacity-0'
-                } transition-all ease-in-out duration-300`}
+                } transition-all ease-in-out duration-300 min-h-72 absolute z-10 p-5 top-0`}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             >
@@ -50,4 +55,11 @@ export const Card = ({
             </a>
         </motion.div>
     );
+};
+
+const getGridRowEnd = (height: any) => {
+    if (height) {
+        return `span ${height}`;
+    }
+    return 'span 1';
 };
