5d3ff0fb958226a3d694b1c7a75628669dabfe2c
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-06-19 00:54:01 +0200

    projects ui fixed
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Projects/Card.tsx
+++ components/Projects/Card.tsx
@@ -23,29 +23,27 @@ export const Card = ({
     return (
         <motion.div
             layout
-            className='relative m-2 text-left border rounded-lg cursor-pointer'
+            className='relative m-2 text-left rounded-lg cursor-pointer'
             style={{
                 width: '100%',
                 maxWidth: '24rem',
                 gridRowEnd: getGridRowEnd(project?.height),
-                gridTemplateColumns: 'repeat(auto-fit, minmax(0, 1fr))',
-                gridTemplateRows: 'repeat(auto-fit, minmax(0, 1fr))',
             }}
-            onClick={() => setSelectedProject(project)}
+            //onClick={() => setSelectedProject(project)}
         >
             <img
                 src={project.background}
                 alt={project.name}
                 className={`${
                     isHovered ? 'blur-sm brightness-50' : ''
-                } transition-all ease-in-out duration-300 rounded-lg`}
+                } transition-all ease-in-out duration-300 rounded-lg border`}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             />
             <a
                 className={`w-full h-full ${
                     isHovered ? 'opacity-100' : 'opacity-0'
-                } transition-all ease-in-out duration-300 min-h-72 absolute z-10 p-5 top-0`}
+                } transition-all ease-in-out duration-300 absolute z-10 p-5 top-0`}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             >
