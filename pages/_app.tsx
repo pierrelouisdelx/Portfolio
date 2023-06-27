195cd13e26703eb65b7d3aa7261ad6af728ae166
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-06-27 21:30:47 +0200

    analytics added and loading screen removed from _app
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import React, { useState, useEffect } from 'react';
-import { LoadingScreen } from '@/components/Preloader/LoadingScreen';
-import { AnimatePresence, motion } from 'framer-motion';
+import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 3000);
-    }, []);
-
     return (
-        <AnimatePresence>
-            {loading && (
-                <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-                    <LoadingScreen />
-                </motion.div>
-            )}
+        <>
             <Component {...pageProps} />
-        </AnimatePresence>
+            <Analytics />
+        </>
     );
 }
