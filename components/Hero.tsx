5097260a5bffd503b53b4a9039eaae9e05add4d5
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-08-23 16:21:57 +0200

    Software engineer and web devlopper fixed
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,14 @@ import { TypeAnimation } from 'react-type-animation';
 import { ArrowDownIcon } from '@heroicons/react/24/solid';
 import { useEffect, useRef } from 'react';
 
-const animationSequence = ['Web Developer Freelance', 1000, 'Computer Vision Engineer', 1000];
+const animationSequence = [
+    'Software Engineer',
+    1000,
+    'Computer Vision Engineer',
+    1000,
+    'Web Developper Freelance',
+    1000,
+];
 
 interface HeroProps {
     setCurrent: (index: any) => void;
@@ -44,7 +51,9 @@ export default function Hero({ setCurrent }: HeroProps) {
             <h1 className='text-4xl font-semibold tracking-wide uppercase md:text-7xl'>
                 Pierre-Louis
             </h1>
-            <h1 className='text-4xl font-semibold tracking-wide uppercase md:text-7xl'>Delcroix</h1>
+            <h1 className='text-4xl font-semibold tracking-wide uppercase md:text-7xl'>
+                Delcroix
+            </h1>
             <div className='flex flex-col items-center md:flex-row'>
                 <h5 className='text-xl tracking-wide uppercase'>I&apos;m a</h5>
                 <h5 className='ml-2 text-xl leading-10 tracking-wide underline uppercase underline-offset-8 decoration-orange-400 decoration-4'>
