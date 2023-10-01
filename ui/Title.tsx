b928ff0fac97bcabd21205556fb667c847dd6ffe
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-10-01 11:47:03 +0200

    reveal added to title component
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- ui/Title.tsx
+++ ui/Title.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from 'react';
+import { Reveal } from '@/components/Reveal';
 
 export default function Title({ children }: { children: ReactNode }) {
     return (
-        <h1
-            className='uppercase tracking-wide font-semibold text-left mt-5 mb-3 underline underline-offset-8'
-            style={{ fontSize: 'clamp(3rem, 5vw, 2.5rem)' }}
-        >
-            {children}
-        </h1>
+        <Reveal>
+            <h1
+                className='uppercase tracking-wide font-semibold text-left mt-5 mb-3 underline underline-offset-8'
+                style={{ fontSize: 'clamp(3rem, 5vw, 2.5rem)' }}
+            >
+                {children}
+            </h1>
+        </Reveal>
     );
 }
