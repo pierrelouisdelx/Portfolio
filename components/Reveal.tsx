43071fa612c1d8009ffb4f96ae0642530a4f7bc9
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-10-01 11:46:46 +0200

    Reveal component added
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

---
+++ components/Reveal.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from 'react';
+import { motion, useInView, useAnimation } from 'framer-motion';
+import classNames from 'classnames';
+
+interface Props {
+    children: JSX.Element;
+    width?: 'fit-content' | '100%';
+}
+
+export const Reveal = ({ children, width = 'fit-content' }: Props) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+
+    const mainControls = useAnimation();
+
+    useEffect(() => {
+        if (isInView) {
+            mainControls.start('visible');
+        }
+    }, [isInView]);
+
+    return (
+        <div
+            className={classNames('relative overflow-hidden', width)}
+            ref={ref}
+        >
+            <motion.div
+                variants={{
+                    hidden: { opacity: 0, y: 75 },
+                    visible: { opacity: 1, y: 0 },
+                }}
+                initial='hidden'
+                animate={mainControls}
+                transition={{ duration: 0.5, delay: 0.25 }}
+            >
+                {children}
+            </motion.div>
+        </div>
+    );
+};
