33a6a3fd437a2f2665178c0b36fda8e92d2785f5
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-10-25 11:53:39 +0200

    navbar underlined added
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -119,13 +119,14 @@ interface LinkProps {
 
 const Link = ({ href, title }: LinkProps) => {
     return (
-        <li>
+        <li className='relative group'>
             <a
                 className='block py-2 pl-3 pr-4 text-gray-900 rounded cursor-pointer hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 '
                 href={href}
             >
                 {title}
             </a>
+            <span className='absolute -bottom-1 left-0 w-0 h-0.5 bg-primary transition-all duration-300 group-hover:w-full'></span>
         </li>
     );
 };
