9795a1896cc43ee3173fbb63ec44ef9bac45efec
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2023-11-11 16:44:55 +0100

    colorScheme removed
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,6 @@ export const metadata: Metadata = {
         'Computer Vision Engineer',
         'Deep Learning Engineer',
     ],
-    colorScheme: 'dark',
 };
 
 export default function RootLayout({
