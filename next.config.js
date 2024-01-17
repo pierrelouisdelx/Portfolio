f7b4cd89a35ff472ea6a8b450c082171ce1f55ef
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2024-01-17 12:48:54 +0100

    domains moved to next config
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,20 +2,53 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: [
-            'raw.githubusercontent.com',
-            'images.emojiterra.com',
-            'cdn.fs.brandfolder.com',
-            'uploads-ssl.webflow.com',
-            'res.cloudinary.com',
-            'parentsonboard.fr',
-            'www.lomed.fr',
-            'www.harmony-promotion.com',
-            'd1hbpr09pwz0sk.cloudfront.net',
-            'www.psychologies.com',
-            'cdn.orbs.cloud'
-        ],
-    },
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.emojiterra.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.fs.brandfolder.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'uploads-ssl.webflow.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'parentsonboard.fr'
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.lomed.fr'
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.harmony-promotion.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'd1hbpr09pwz0sk.cloudfront.net'
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.psychologies.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.orbs.cloud'
+            }
+        ]
+    }
 };
 
 module.exports = nextConfig;
