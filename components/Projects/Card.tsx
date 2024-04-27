b6cd2142dec4f0c584acd5f66f10c6e150209d0f
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2024-04-26 20:51:15 -0400

    Pin working
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- components/Projects/Card.tsx
+++ components/Projects/Card.tsx
@@ -1,6 +1,7 @@
 import { Project } from '@/data/projects';
 import clsx from 'clsx';
 import Image from 'next/image';
+import { PinContainer } from './Pin';
 
 export const Card = ({
     project,
@@ -15,29 +16,35 @@ export const Card = ({
     return (
         <div
             className={clsx(
-                'cursor-pointer col-span-1 relative hover:scale-105 transition-all duration-300',
-                h > 1 ? 'row-span-3' : 'row-span-1'
+                'cursor-pointer col-span-1 relative hover:scale-105 transition-all duration-300 w-[500px]',
+                h > 1 ? 'row-span-3 h-[950px]' : 'row-span-1 h-[300px]'
             )}
             onClick={() => setSelectedProject(project)}
         >
-            <Image
-                src={project.background as string}
-                alt={project.name}
-                className='rounded-lg'
-                width={w * 500}
-                height={h * 300}
-            />
+            <PinContainer
+                title={project.name}
+                description={project.description}
+                h={h}
+            >
+                <Image
+                    src={project.background as string}
+                    alt={project.name}
+                    className='rounded-lg'
+                    width={w * 500}
+                    height={h * 300}
+                />
 
-            <p className='flex flex-wrap justify-left absolute bottom-0 left-2'>
-                {project.technologies.map((tech) => (
-                    <span
-                        className='bg-primary/[0.8] px-2 py-1 rounded-lg mr-2 mb-2'
-                        key={tech}
-                    >
-                        {tech}
-                    </span>
-                ))}
-            </p>
+                <p className='flex flex-wrap justify-left absolute bottom-0 left-2'>
+                    {project.technologies.map((tech) => (
+                        <span
+                            className='bg-primary/[0.8] px-2 py-1 rounded-lg mr-2 mb-2'
+                            key={tech}
+                        >
+                            {tech}
+                        </span>
+                    ))}
+                </p>
+            </PinContainer>
         </div>
     );
 };
